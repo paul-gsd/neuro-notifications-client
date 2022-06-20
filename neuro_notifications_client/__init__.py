26485d9d8e4b790bc87acb56773d2e40e1d9ff52
Ivan Zubenko <vanya.zubenko@gmail.com>
2022-06-20 12:44:59 +0300

    export Welcome notification

--- neuro_notifications_client/__init__.py
+++ neuro_notifications_client/__init__.py
@@ -7,6 +7,7 @@ from .notifications import (
     JobTransition,
     QuotaResourceType,
     QuotaWillBeReachedSoon,
+    Welcome,
 )
 
 __all__ = [
@@ -18,4 +19,5 @@ __all__ = [
     "QuotaWillBeReachedSoon",
     "QuotaResourceType",
     "CreditsWillRunOutSoon",
+    "Welcome",
 ]
